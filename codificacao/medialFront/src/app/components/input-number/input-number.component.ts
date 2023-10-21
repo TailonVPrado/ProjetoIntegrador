7b6dce0616b76851ffeffb796bbff4b596acc22f
TailonVPrado <tailon.prado@edu.unipar.br>
2023-10-21 18:06:55 -0300

    Alterações no input number para melhorar a digitação d usuario (antes quendo ele comecava o valor comecava a ser acrescentado pelos decimais, e agora é pelo inteiro, vai para casa decimal apenas quando ele preciona a virgula)

--- codificacao/medialFront/src/app/components/input-number/input-number.component.ts
+++ codificacao/medialFront/src/app/components/input-number/input-number.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { CurrencyMaskInputMode } from 'ngx-currency';
 import { Properties } from 'src/app/models/interface/properties.model';
 
 @Component({
@@ -11,20 +12,41 @@ export class InputNumberComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.maskOptions = {
+      prefix: "",
+      suffix: "",
+      thousands: "",
+      decimal: ",",
+      precision: this.decimal ? 1 : 0,
+      allowNegative: true,
+      allowZero: true,
+      nullable: true,
+      align: this.align,
+      inputMode: CurrencyMaskInputMode.NATURAL,
+    };
   }
+
   @Output() itemSelecionado: EventEmitter<any> = new EventEmitter<any>();
   @Input() properties: Properties | undefined;
   @Input() isGrid: boolean = false;
   @Input() decimal : boolean = false;
   @Output() mxModelChange: EventEmitter<any> = new EventEmitter<any>();
-  @Input() align : string = '';
+  @Input() align : string = 'right';
 
-  actualValue: any;
+  actualValue : any;
+  maskOptions : any;
+
+  @ViewChild('inputElement') inputElement : ElementRef | any;
+
 
   @Input() set mxModel(val: any) {
     this.actualValue = val;
     this.mxModelChange.emit(val);
   } get mxModel() {
     return this.actualValue;
+  }
+
+  onFocus(){
+    this.inputElement.nativeElement.select();
   }
 }
